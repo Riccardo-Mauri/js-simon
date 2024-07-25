722a223474456aa53a5d31f973edbaa9e1c10109
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-25 14:06:02 +0200

    creazione ciclo dei numeri casuali creato e stampato in console con un array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-console.log('ciao mondo!')
+
+//metto delle variabili da riutilizzare
+const minNumber = 1;
+const maxNumber = 30;
+const randomNumbers = [];
+//creo un ciclo delle mie variabili in questo caso i numeri
+for (let i = 0; i < 5; i++) {
+    const num = getRandomNumber(minNumber, maxNumber);
+    console.log('num', num, typeof num);
+    randomNumbers.push(num);
+}
+console.log('randomNumbers', randomNumbers, typeof randomNumbers);
+
+
+
+
+
+
+
+
+
+
+/*
+FUNZIONI 
+*/
+
+//genero i numeri random che mi servono
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
