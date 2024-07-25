87b07727ced33fd706198edc6397a9d02e8df857
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-25 14:15:35 +0200

    personalizzazione e inserimento numeri nell' HTML

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@
 const minNumber = 1;
 const maxNumber = 30;
 const randomNumbers = [];
+const randomnumContainer = document.getElementById('numbers-container');
 //creo un ciclo delle mie variabili in questo caso i numeri
 for (let i = 0; i < 5; i++) {
     const num = getRandomNumber(minNumber, maxNumber);
     console.log('num', num, typeof num);
     randomNumbers.push(num);
+    //salvo i numeri in pagina
+    randomnumContainer.innerHTML += `<span>${num}</span>`
+
 }
 console.log('randomNumbers', randomNumbers, typeof randomNumbers);
+
 
 
 
